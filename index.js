60db99fae730726ec184b3895424c979e7a132e6
Thanh Nguyen <thn21@t14s>
2021-04-07 18:14:18 -0400

    basic functional rest api service

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
 const express = require('express');
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
+
+const SensorData = sequelize.define('sensor-data', {
+    serial: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    temperature: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    }
+})
 
 const app = express();
 app.use(express.json());
 
 const dataList = [];
 
-app.get('/hello', (req, res) => {
+app.get('/data', async (req, res) => {
+    const allData = await SensorData.findAll();
     res.status(200).send(dataList);
+    return;
 
 });
 
-app.post('/data', (req, res) => {
+app.post('/data', async (req, res) => {
     let data = req.body;
+    const sensorData = await SensorData.create(data);
     dataList.push(data);
-    res.status(201).send(data);
+    res.status(201).send(sensorData);
+    return;
 
 });
 app.listen({ port: 8080 }, () => {
     try{
         sequelize.authenticate();
         console.log('Connected to database')
+
+        sequelize.sync({ alter: true });
+        console.log('Sync to database')
     }
     catch(error){
         console.log('Could not connected to database', error)
