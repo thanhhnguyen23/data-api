1cb2995b2e36c2b73e0b33878d9d0907e372932c
Thanh Nguyen <thn21@t14s>
2021-04-07 17:44:35 -0400

    add db dependencies; wire db to app

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres'
+});
 
 const app = express();
 app.use(express.json());
@@ -17,5 +22,12 @@ app.post('/data', (req, res) => {
 
 });
 app.listen({ port: 8080 }, () => {
+    try{
+        sequelize.authenticate();
+        console.log('Connected to database')
+    }
+    catch(error){
+        console.log('Could not connected to database', error)
+    }
     console.log('Server is running');
 })
