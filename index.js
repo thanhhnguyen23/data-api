671979eb6e200dd7aa07e22dd72b5e5cb15dc932
Thanh Nguyen <thn21@t14s>
2021-04-07 17:20:17 -0400

    add /data route

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
+const dataList = [];
 
 app.get('/hello', (req, res) => {
-    res.status(200).send('hello');
+    res.status(200).send(dataList);
 
 });
 
+app.post('/data', (req, res) => {
+    let data = req.body;
+    dataList.push(data);
+    res.status(201).send(data);
+
+});
 app.listen({ port: 8080 }, () => {
     console.log('Server is running');
 })
